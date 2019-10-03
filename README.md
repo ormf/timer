9d164257e2516ce817f8b38a0f67d4b33aa80f3e
Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
2019-10-03 11:08:15 +0200

    erster commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Javascript Timer
+
+Code for a Timer which works in a Browser.
+
+Features:
+
+- Timer can count up or down.
+
+- Acoustic and optical signal.
+
+- Acoustic signal can be muted.
+
+- Timer can be set to repeat mode (e.g. for gym exercises), autostop
+  or non-stop (continuing to show the time after chiming).
+
+- Background turns red if time limit is exceeded.
+
+Written by Ugo Finnendahl and Orm Finnendahl. Licensed under the GPL
+v3.0 license or later.
---
+++ timer.js
@@ -0,0 +1,210 @@
+/*
+
+timer.js
+
+**********************************************************************
+Copyright (c) 2019 Ugo Finnendahl <ugogon@hotmail.de>
+                   Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
+
+Revision history: See git repository.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the Gnu Public License, version 2 or
+later. See https://www.gnu.org/licenses/gpl-2.0.html for the text
+of this agreement.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+*/
+
+class Timer {
+    constructor(display_element, sound_element) {
+        this.total = 5;
+        this.todo = undefined;
+        this.target = undefined;
+        this.timer = undefined;
+        this.last_tick = this.now();
+        this.up = true;
+        this.running = false;
+        this.loop = 0;
+        this.mute = false;
+        this.sound = sound_element;
+        this.timedisplay = display_element;
+        this.reset();
+        setInterval(this.tick.bind(this), 1000);
+    }
+    now(){
+        return new Date().getTime();
+    }
+    set(target){
+        var old = this.total;
+        this.total = target;
+        if (this.running){
+            this.todo += (target-old)*1000;
+            if (this.todo < 0){
+                this.todo = 0;
+            }
+            this.displaytime();
+        } else {
+            this.reset();
+        }
+    }
+    ring(){
+        this.sound.play();
+    }
+    stopRing(){
+        this.sound.pause();
+    }
+    tick(){
+        if (this.running) {
+            var now = this.now();
+            this.todo -= (now-this.last_tick);
+            console.log(this.todo);
+            console.log(Math.round(this.todo/100));
+            if (Math.round(this.todo/1000) == 0) {
+                if (!this.mute) {
+                    this.ring();
+                    setTimeout(function(){this.stopRing()}.bind(this), 1000);
+                }
+                switch (this.loop) {
+                case 0:
+                    this.reset();
+                    break;
+                case 1:
+                    this.running = false;
+                    update_button(document.getElementById('ttoggle'), this.running);
+                    this.displaytime();
+                    break;
+                default:
+                    this.displaytime();                    
+                }
+            } else {
+                this.displaytime();
+            }
+            this.last_tick = now;
+        }
+    }
+    displaytime(){
+        var time = Math.round(this.todo/1000);
+        if (this.up){
+            time = this.total-time;
+        }
+        var hours = Math.floor((time % (60 * 60 * 24)) / (60 * 60));
+        var minutes = Math.floor((time % (60 * 60)) / 60);
+        var seconds = Math.floor(time % 60);
+        [hours, minutes, seconds] = [hours, minutes, seconds].map((a) => a.toLocaleString('en', {minimumIntegerDigits:2}))
+        this.timedisplay.innerHTML = `${hours}:${minutes}:${seconds}`;
+    }
+    start(){
+        this.last_tick = this.now();
+        this.running = true;
+    }
+    pause(){
+        this.todo -= (this.now()-this.last_tick);
+        this.running = false;
+        this.displaytime();
+    }
+    reset(){
+        this.todo = this.total*1000;
+        this.displaytime();
+    }
+}
+
+//--------------------- View --------------------------------------------------
+
+function update_view(){
+    update_button(document.getElementById('ttoggle'), timer.running);
+    update_button(document.getElementById('tdirection'), timer.up);
+    update_radio(document.getElementById('tloop'), timer.loop);
+    update_button(document.getElementById('tmute'), timer.mute);
+}
+
+function update_radio(button, condition){
+    switch (condition) {
+    case 0 : { button.style.backgroundImage = `url(${button.dataset.url0})`; break }
+    case 1 : { button.style.backgroundImage = `url(${button.dataset.url1})`; break }
+    case 2 : { button.style.backgroundImage = `url(${button.dataset.url2})`; break }
+    }
+}
+
+function update_button(button, condition){
+    if (condition) {
+        button.style.backgroundImage = `url(${button.dataset.onurl})`;
+    } else {
+        button.style.backgroundImage = `url(${button.dataset.offurl})`;
+    }
+}
+
+function update_selection(hr_obj, hm_obj, hs_obj){
+    var hours = Math.floor((timer.total % (60 * 60 * 24)) / (60 * 60));
+    var minutes = Math.floor((timer.total % (60 * 60)) / 60);
+    var seconds = Math.floor(timer.total % 60);
+    hr_obj.value = hours;
+    hm_obj.value = minutes;
+    hs_obj.value = seconds;
+}
+
+function recalcTotal(hr_obj, hm_obj, hs_obj){
+    timer.set(parseInt((hr_obj.value * 3600),10) + parseInt((hm_obj.value * 60),10) + parseInt(hs_obj.value, 10));
+}
+
+function createSel(max) {
+    var selector = document.createElement("select");
+    for (var i=0; i<=max; i++) {
+        var opt = document.createElement("option");
+        opt.value = i;
+        opt.innerHTML = i.toLocaleString('en', {minimumIntegerDigits:2});
+        selector.appendChild(opt);
+    }
+    return selector
+}
+
+//--------------------- Init --------------------------------------------------
+
+var timer;
+
+function init(){
+    timer = new Timer(document.getElementById("timedisplay"),document.getElementById("alarm-sound"));
+
+    document.getElementById('ttoggle').addEventListener("click", function(){
+        if (timer.running) {
+            timer.pause();
+        } else {
+            timer.start();
+        }
+        update_button(this, timer.running);
+    });
+    document.getElementById('treset').addEventListener("click", function(){
+        timer.reset();
+    });
+    document.getElementById('tdirection').addEventListener("click", function(){
+        timer.up = !timer.up;
+        timer.displaytime()
+        update_button(this, timer.up);
+    });
+    document.getElementById('tloop').addEventListener("click", function(){
+        timer.loop = (timer.loop+1) % 3;
+        update_radio(this, timer.loop);
+    });
+    document.getElementById('tmute').addEventListener("click", function(){
+        timer.mute = !timer.mute;
+        update_button(this, timer.mute);
+    });
+    update_view();
+
+    var thr = createSel(23);
+    document.getElementById("tpick-h").appendChild(thr);
+    var thm = createSel(59);
+    document.getElementById("tpick-m").appendChild(thm);
+    var ths = createSel(59);
+    document.getElementById("tpick-s").appendChild(ths);
+
+    update_selection(thr, thm, ths);
+
+    [thr, thm, ths].map((a) => a.addEventListener("change", function(){recalcTotal(thr, thm, ths)}));
+}
+
+window.addEventListener("load", init);
